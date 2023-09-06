0d925475b89d78c5edd46b004a6afe47afb83929
Becky <bjentwistle@gmail.com>
2023-09-06 23:00:35 +0100

    improved code for number formatting to keep it DRY

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
 import { useEffect, useState } from "react";
 import IndividualPage from "./components/IndividualPage";
 
+//mock data to test formatting correctly
+const copiedCampaignsData = [
+  {
+  "id": 16346145,
+  "title": "Jan - Mar 2023",
+  "enddate": "31/03/2023",
+  "totalImpressions": 111300000,
+  "totalResponse": 1100800 
+  },
+  {
+  "id": 83349140,
+  "title": "Apr - Jun 2023",
+  "startdate": "01/04/2023",
+  "enddate": "30/06/2023",
+  "totalImpressions": 12291000, //added some zeros
+  "totalResponse": 1848700
+  },
+  {
+  "id": 19343106,
+  "title": "Jul - Sept 2023",
+  "startdate": "01/07/2023",
+  "enddate": "30/09/2023",
+  "totalImpressions": 12498000000,
+  "totalResponse": 11239
+  },
+  {
+  "id": 63129199,
+  "title": "Oct - Dec 2023",
+  "startdate": "01/10/2023",
+  "enddate": "31/12/2023"
+  }
+  ]
+  
 function App() {
 
   const apiUrl = "https://cclan.s3.eu-west-1.amazonaws.com/campaigns.json";
@@ -12,7 +45,7 @@ function App() {
       const res = await fetch(url);
       const data = await res.json();
 
-      // Format the data immediately after fetching
+      //Format the data immediately after fetching
       const formattedData = formatData(data);    
 
     } catch (error) {
@@ -43,23 +76,50 @@ function App() {
       }
   
     }
-      switch (true) {
-        case totalImpressions >= 1000000000: // For billions
-          totalImpressions = (totalImpressions / 1000000000).toFixed(1) + 'b';
-          totalResponse = (totalResponse / 1000000000).toFixed(1) + 'b';
-          break;
-        case totalImpressions >= 1000000: // For millions
-          totalImpressions = (totalImpressions / 1000000).toFixed(1) + 'm';
-          totalResponse = (totalResponse / 1000000).toFixed(1) + 'm';
-          break;
-        case totalImpressions >= 1000: // For thousands
-          totalImpressions = (totalImpressions / 1000).toFixed(1) + 'k';
-          totalResponse = (totalResponse / 1000).toFixed(1) + 'k';
-          break;
-        default:
-          totalImpressions = totalImpressions;
-          totalResponse = totalResponse;
+
+      function formatNumberWithSuffix(number) {
+        if (number >= 1000000000) {
+          return (number / 1000000000).toFixed(1) + 'b';
+        } else if (number >= 1000000) {
+          return (number / 1000000).toFixed(1) + 'm';
+        } else if (number >= 1000) {
+          return (number / 1000).toFixed(1) + 'k';
+        } else {
+          return number;
+        }
       }
+    //more concise way of formatting numbers than duplicating the switch case. Also allows for expansion if other data is added to the tables.
+
+      totalImpressions = formatNumberWithSuffix(totalImpressions);
+      totalResponse = formatNumberWithSuffix(totalResponse);
+    
+      // switch (true) {
+      //   case totalImpressions >= 1000000000: // For billions
+      //     totalImpressions = (totalImpressions / 1000000000).toFixed(1) + 'b';
+      //     break;
+      //   case totalImpressions >= 1000000: // For millions
+      //     totalImpressions = (totalImpressions / 1000000).toFixed(1) + 'm';
+      //     break;
+      //   case totalImpressions <= 1000: // For thousands
+      //     totalImpressions = (totalImpressions / 1000).toFixed(1) + 'k';
+      //     break;
+      //   default:
+      //     totalImpressions = totalImpressions;
+      // }
+      // switch (true) {
+      //   case totalResponse >= 1000000000: // For billions
+      //     totalResponse = (totalResponse / 1000000000).toFixed(1) + 'b';
+      //     break;
+      //   case totalResponse >= 1000000: // For millions
+      //     totalResponse = (totalResponse / 1000000).toFixed(1) + 'm';
+      //     break;
+      //   case totalResponse >= 1000: // For thousands
+      //   totalResponse = (totalResponse / 1000).toFixed(1) + 'k';
+      //     break;
+      //   default:
+      //     totalResponse = totalResponse;
+          
+      // }
     
       return {
         ...campaign,
@@ -73,6 +133,8 @@ function App() {
 
   useEffect(()=>{
     fetchFunction(apiUrl)
+    //formatData(copiedCampaignsData);
+
   }, [])
 
 
